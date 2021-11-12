1866cb331b40f0396701f095bd8b431d50999a4b
CCK <whckdrl52@naver.com>
2021-11-12 12:46:03 +0900

    delete a file(emp)

--- src/main/java/kr/co/dong/emp/EmpDAO.java
+++
@@ -1,8 +0,0 @@
-package kr.co.dong.emp;
-
-import java.util.List;
-
-public interface EmpDAO {
-
-	public int empCount() throws Exception;
-}
--- src/main/java/kr/co/dong/emp/EmpService.java
+++
@@ -1,8 +0,0 @@
-package kr.co.dong.emp;
-
-import java.util.List;
-
-public interface EmpService {
-
-	public int empCount() throws Exception;
-}
